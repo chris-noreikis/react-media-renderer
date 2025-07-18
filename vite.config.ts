b846d94538f60841c014aebadf07423a2e8daf86
Chris Noreikis <chris.nore@gmail.com>
2025-07-18 08:31:20 -0500

    bundle app into ejs/cjs/umd output for installs

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,30 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [react()],
-  root: './dev',
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
-    outDir: '../dist',
+    lib: {
+      entry: resolve(__dirname, 'src/index.tsx'),
+      name: 'ReactMediaRenderer',
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        exports: 'named',
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
   },
 });
