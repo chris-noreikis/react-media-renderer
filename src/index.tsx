87dfe1dfce72c3202a14f40c42032b0c9c85918f
Chris Noreikis <chris.nore@gmail.com>
2025-07-19 13:25:22 -0500

    Add JSDoc comments

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { detectMediaType } from './detectionStrategies';
 import { DetectionStrategy, MediaType } from './types';
 
-export interface MediaRendererProps extends React.HTMLAttributes<HTMLElement> {
+interface MediaRendererProps extends React.HTMLAttributes<HTMLElement> {
   src: string;
   imageProps?: React.ImgHTMLAttributes<HTMLImageElement>;
   videoProps?: React.VideoHTMLAttributes<HTMLVideoElement>;
@@ -11,15 +11,27 @@ export interface MediaRendererProps extends React.HTMLAttributes<HTMLElement> {
   renderVideo?: (props: { src: string; videoProps?: React.VideoHTMLAttributes<HTMLVideoElement>; [key: string]: any }) => React.ReactNode;
 }
 
-const MediaRenderer: React.FC<MediaRendererProps> = ({ 
-  src, 
-  imageProps, 
-  videoProps, 
+/**
+ * A React component that automatically detects and renders media files as either images or videos.
+ *
+ * @param {string} src - The source URL of the media file to be rendered
+ * @param {React.ImgHTMLAttributes<HTMLImageElement>} imageProps - Additional props to pass to the img element when rendering images
+ * @param {React.VideoHTMLAttributes<HTMLVideoElement>} videoProps - Additional props to pass to the img element when rendering videos
+ * @param {DetectionStrategy} detectionStrategy - Choose how to detect whether src is an image or video.  One of {fileExtension, contentType}.  Defaults to 'fileExtension'.
+ * @param {function} renderImage - Custom render function for images. Receives {@link MediaRendererProps}
+ * @param {function} renderVideo - Custom render function for videos. Receives {@link MediaRendererProps}
+ * @param {React.HTMLAttributes<HTMLElement>} props - Arbitrary HTMLElement props spread to underlying element
+ * @returns A rendered media element (img or video) or null if media type cannot be determined
+ */
+const MediaRenderer: React.FC<MediaRendererProps> = ({
+  src,
+  imageProps,
+  videoProps,
   detectionStrategy = 'fileExtension',
   renderImage,
   renderVideo,
-  ...props 
-}) => {
+  ...props
+}: MediaRendererProps) => {
   const [mediaType, setMediaType] = useState<MediaType | null>(null);
 
   useEffect(() => {
